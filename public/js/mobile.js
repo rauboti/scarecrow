dced6ea2dc843de704154ffc162909239225a2e0
Gaute Rauboti <gaute@rauboti.net>
2018-08-27 00:16:12 +0200

    some cleanup in css, preparations for responsive design, Progression page made responsive

---
+++ public/js/mobile.js
@@ -0,0 +1,30 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+function deviceAdjustment() {
+  if( isMobile.any() ) {
+    $('.col-container').addClass('col-container-mobile');
+    $('.container').addClass('container-mobile');
+  } else {
+    $('.col-container').addClass('col-container-desktop');
+    $('.container').addClass('container-desktop');
+  }
+}
