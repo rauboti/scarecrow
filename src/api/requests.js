28e38a9963009c06ad3b255df268fdb7f82cb9e0
Gaute Rauboti <gaute@rauboti.net>
2019-05-04 00:57:02 +0200

    extra logging?

--- src/api/requests.js
+++ src/api/requests.js
@@ -12,11 +12,11 @@ const local = module.exports = {
         character: {
             getAll: async function(token) {
                 var characters = {}
-                debug(token)
+                console.log(token)
                 url = 'https://eu.api.blizzard.com/wow/user/characters?access_token=' + token;
 
                 var response = await getData(url);
-                debug(response)
+                console.log(response)
                 data = response['characters'] // Blizzard returns the json named characters; this makes the code easier to read below
                 for (var char in data) {
                     !(characters[data[char].realm]) && (characters[data[char].realm] = {});
